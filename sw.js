ba2a48007339f19245654c3776b95b1fbfb84741
quunnnboy <quintenbeke1@gmail.com>
2026-08-28 17:23:07 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,30 @@
-// const CACHE_NAME = "dicetower-cache-v3";
-// const ASSETS_TO_CACHE = [
-//   "./",
-//   "./index.html",
-//   "./style.css",
-//   "./script.js",
-//   "./manifest.json"
-//   // Vergeet niet hier later je iconen toe te voegen als je die hebt!
-//   // "./icon-192.png",
-//   // "./icon-512.png"
-// ];
+const CACHE_NAME = "dicetower-cache-v3";
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
+  // Vergeet niet hier later je iconen toe te voegen als je die hebt!
+  // "./icon-192.png",
+  // "./icon-512.png"
+];
 
-// // Installeer de bestanden in het geheugen
-// self.addEventListener("install", event => {
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then(cache => {
-//       console.log("Bestanden gecached voor offline gebruik!");
-//       return cache.addAll(ASSETS_TO_CACHE);
-//     })
-//   );
-// });
+// Installeer de bestanden in het geheugen
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Bestanden gecached voor offline gebruik!");
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
 
-// // Laad de pagina uit het geheugen als er geen internet is
-// self.addEventListener("fetch", event => {
-//   event.respondWith(
-//     caches.match(event.request).then(response => {
-//       return response || fetch(event.request);
-//     })
-//   );
-// });
+// Laad de pagina uit het geheugen als er geen internet is
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
